c651d2354d168d00c1175d248f6dbfacfdac558b
masbaghkondori <Masoomeh.Baghban@yahoo.com>
2018-04-24 12:28:14 +0200

    first commit

---
+++ src/main/java/com/gkatzioura/springcamel/SpringCamelApplication.java
@@ -0,0 +1,14 @@
+package com.gkatzioura.springcamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.common.base.Strings;
+
+@SpringBootApplication
+public class SpringCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamelApplication.class, args);
+	}
+}
